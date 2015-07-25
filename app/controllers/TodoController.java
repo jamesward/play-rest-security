4b1968fbabdaa4b3a7c48f727d6953417cc143ce
porter <brian@porter.net>
2015-07-25 19:59:50 +0200

    Added Swagger for API Documentation

--- app/controllers/TodoController.java
+++ app/controllers/TodoController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.wordnik.swagger.annotations.*;
 import models.Todo;
 import play.Logger;
 import play.data.Form;
@@ -9,13 +10,41 @@ import java.util.List;
 
 import static play.libs.Json.toJson;
 
+@Api(value = "/api/todos", description = "Operations with Todos")
 @Security.Authenticated(Secured.class)
 public class TodoController extends Controller {
 
+    @ApiOperation(value = "get All Todos",
+            notes = "Returns List of all Todos",
+            response = Todo.class,
+            httpMethod = "GET")
     public static Result getAllTodos() {
         return ok(toJson(models.Todo.findByUser(SecurityController.getUser())));
     }
 
+    @ApiOperation(
+            nickname = "createTodo",
+            value = "Create Todo",
+            notes = "Create Todo record",
+            httpMethod = "POST",
+            response = Todo.class
+    )
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(
+                            name = "body",
+                            dataType = "Todo",
+                            required = true,
+                            paramType = "body",
+                            value = "Todo"
+                    )
+            }
+    )
+    @ApiResponses(
+            value = {
+                    @com.wordnik.swagger.annotations.ApiResponse(code = 400, message = "Json Processing Exception")
+            }
+    )
     public static Result createTodo() {
         Form<models.Todo> form = Form.form(models.Todo.class).bindFromRequest();
         if (form.hasErrors()) {
--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ scalaVersion := "2.11.1"
 libraryDependencies ++= Seq(
   javaCore,
   javaJdbc,
-  javaEbean
+  javaEbean,
+  "com.wordnik" %% "swagger-play2" % "1.3.12" exclude("org.reflections", "reflections"),
+  "org.reflections" % "reflections" % "0.9.8" notTransitive (),
+  "org.webjars" % "swagger-ui" % "2.1.8-M1"
 )
